30b1b526982ab637c8338e8906fa1afb59037eff
Dreamsplutox <arnaudsimon091@gmail.com>
2019-07-09 19:15:32 +0200

    add goblin ennemy

--- game/app.py
+++ game/app.py
@@ -56,12 +56,51 @@ class projectile(object):
 	def draw(self, win):
 		pygame.draw.circle(win, self.color, (self.x, self.y), self.radius) #add , 1 to fill differently
 
+class enemy(object):
+	walkRight = [pygame.image.load('images/R1E.png'), pygame.image.load('images/R2E.png'), pygame.image.load('images/R3E.png'), pygame.image.load('images/R4E.png'), pygame.image.load('images/R5E.png'), pygame.image.load('images/R6E.png'), pygame.image.load('images/R7E.png'), pygame.image.load('images/R8E.png'), pygame.image.load('images/R9E.png'), pygame.image.load('images/R10E.png'), pygame.image.load('images/R11E.png')]
+	walkLeft = [pygame.image.load('images/L1E.png'), pygame.image.load('images/L2E.png'), pygame.image.load('images/L3E.png'), pygame.image.load('images/L4E.png'), pygame.image.load('images/L5E.png'), pygame.image.load('images/L6E.png'), pygame.image.load('images/L7E.png'), pygame.image.load('images/L8E.png'), pygame.image.load('images/L9E.png'), pygame.image.load('images/L10E.png'), pygame.image.load('images/L11E.png')]
 
+	def __init__(self, x, y, width, height, end):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.end = end
+		self.path = [self.x, self.end]
+		self.walkCount = 0
+		self.vel = 3
+
+	def draw(self, win):
+		self.move()
+		if self.walkCount + 1 >= 33:
+			self.walkCount = 0
+
+		if self.vel > 0:
+			win.blit(self.walkRight[self.walkCount//3], (self.x, self.y))
+			self.walkCount += 1
+		else:
+			win.blit(self.walkLeft[self.walkCount//3], (self.x, self.y))
+			self.walkCount += 1
+
+	def move(self):
+		if self.vel > 0:
+			if self.x + self.vel < self.path[1]:
+				self.x += self.vel
+			else:
+				self.vel = self.vel * -1
+				self.walkCount = 0
+		else:
+			if self.x - self.vel > self.path[0]:
+				self.x += self.vel
+			else:
+				self.vel = self.vel * -1
+				self.walkCount = 0
 
 def redrawGameWindow():
 	global walkCount
 	win.blit(bg, (0,0))
 	man.draw(win)
+	goblin.draw(win)
 	for bullet in bullets:
 		bullet.draw(win)
 	pygame.display.update()
@@ -69,6 +108,7 @@ def redrawGameWindow():
 
 #mainloop
 man = player(300, 410, 64, 64)
+goblin = enemy(100, 410, 64, 64, 450)
 bullets = []
 run = True
 
@@ -126,6 +166,8 @@ while run:
 			man.jumpCount = 10
 
 	redrawGameWindow()
+
+
 
 
 pygame.quit()
