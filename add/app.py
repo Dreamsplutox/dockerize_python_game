e4354b9e6946e0abe1a4704436554492bedc36d0
Nathanaël <nat.nez.ren@gmail.com>
2019-07-09 19:06:14 +0200

    update of the game Engine : the square can't go out the window

--- add/app.py
+++ add/app.py
@@ -6,15 +6,18 @@ win = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("First Game in Python")
 
 x = 50
-y = 50
+y = 450
 width = 40
 height = 60
 vel = 5
 
+isJump = False
+jumpCount = 10
+
 run = True
 
 while run:
-	pygame.time.delay(100)
+	pygame.time.delay(50)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -22,14 +25,29 @@ while run:
 	
 	keys = pygame.key.get_pressed()
 
-	if keys[pygame.K_LEFT]:
+	if keys[pygame.K_LEFT] and x > vel:
 		x -= vel
-	if keys[pygame.K_RIGHT]:
+	if keys[pygame.K_RIGHT] and x < 800 - width - vel:
 		x += vel
-	if keys[pygame.K_UP]:
-		y -= vel
-	if keys[pygame.K_DOWN]:
-		y += vel
+	if not(isJump):
+		if keys[pygame.K_UP] and y > vel:
+			y -= vel
+		if keys[pygame.K_DOWN] and y < 800 - height - vel:
+			y += vel
+		if keys[pygame.K_SPACE]:
+			isJump = True
+	else:
+		if jumpCount >= - 10:
+			neg = 1
+			if jumpCount < 0:
+				neg = -1
+			y -= (jumpCount ** 2) * 0.5 * neg
+			jumpCount -= 1
+		else:
+			isJump = False
+			jumpCount = 10
+
+
 
 	win.fill((0,0,0))
 	pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
